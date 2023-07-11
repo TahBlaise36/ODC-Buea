aa5cd3d69300d8f69afe7ef6541436dfb2b31951
TahBlaise36 <tahblaise36@gmail.com>
2023-07-11 11:28:05 +0100

    initial commit

---
+++ introduction.js
@@ -0,0 +1,52 @@
+// This is a sigle line comment
+/*
+this is a multiline comment
+Here is another comment
+*/
+
+// Variable declarations
+
+// var initialVariable
+// var myVariable = 10
+// let myVariableName = "hello"
+// const variable = 12
+
+// myVariable = 15
+// myVariableName = "Good morning"
+// // variable = 34   can't work since a value can't be assigned to a constant variable
+
+// console.log(typeof(initialVariable));
+// console.log(typeof(myVariable));
+// console.log(myVariable);
+// console.log(myVariableName);
+// console.log(variable);
+
+// ************* Operators in Javascript
+// *** Addition, subtraction, multiplication, division, and modulo
+
+// var a = 2, b = 10
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+/* *********** increment and decrement ************ */
+// let temp1 = 0
+// let temp2 = 0
+// var increment = 3, decrement = 12;
+// console.log(increment, decrement);
+// 
+// increment = increment + 1;
+// increment++;
+// increment+=1;
+// 
+// temp1  = ++increment;
+// temp2 = increment++;
+// console.log(temp2);
+// 
+// decrement = decrement - 1;
+// decrement--;
+// decrement-=1;
+// console.log(increment, decrement);
