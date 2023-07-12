bbccad09fa99f9f3b6a972c30c512837ffa0fb55
TahBlaise36 <tahblaise36@gmail.com>
2023-07-12 12:02:45 +0100

    finished working with functions

--- introduction.js
+++ introduction.js
@@ -93,3 +93,68 @@ Here is another comment
 // console.log(myArr[6]);
 // console.log(myArr);
 // console.log(last);
+
+/** Working with fuction */
+
+// The common way of declaring a function
+
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+
+// console.log(addTwoNumbers(2, 5));
+
+// function sumOfNumbers(...number){
+//     let sum = 0;
+//     number.forEach((a) => {
+//         sum += a;
+//     });
+
+//     return sum;
+// }
+
+// console.log(sumOfNumbers(2, 5, 4, 8, 3));
+
+// Using ES6 syntax
+
+// const subtractTwoNumbers (a, b) => {
+//     return a - b;
+// };
+
+// console.log(subtractTwoNumbers(7, 3));
+
+// function numberOfPears(arr){
+
+//     let count = []
+//     arr.forEach((a) => {
+//         let num = 0;
+//         if (a){
+//             num++;
+//         }
+//     });
+
+//     return sum;
+// }
+
+// console.log(sumOfNumbers[2, 5, 4, 8, 3]);
+
+// function countPairs(numbers) {
+//     let pairs = 0;
+//     let counts = [];
+//     for (let i =  0; i < numbers.length; i++){
+//         numbers.map((num) => {
+//             if (numbers[0] === num) {
+//                 counts.push(num);
+//             }
+//         });
+
+//         pairs += Math.floor(counts.length / 2);
+//         counts = [];
+//         // Creating a new array that doesn't contain the zeroth element
+//         numbers = numbers.filter((num) => num !== numbers[0]);
+//     }
+//     return pairs;
+// }
+
+// console.log(countPairs([1, 6, 5, 8, 5, 5, 5]));
+
